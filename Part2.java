ee10be2d528ec0ab6d81864976442de25d91e0ca
Mohammed Alshehri <moe.ghanim.alshehri@gmail.com>
2018-09-29 19:06:11 -0400

    added arrayListTimeAndSum and vectorTimeAndSum

--- Part2.java
+++ Part2.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Vector;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+import java.util.Iterator;
 
 public class Part2 {
-	
+
 	private static double arrayTime (int size) {
 		int[] array = new int[size];
 		long StartTime;
-		long endTime; 
+		long endTime;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
 		for (int i = 0; i < 1000; i++){
@@ -21,16 +23,16 @@ public class Part2 {
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+
 	private static double arrayTimeAndSum (int size) {
 		int[] array = new int[size];
 		long StartTime;
 		long endTime;
-		long endTimeAdd; 
+		long endTimeAdd;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
 		int sum = 0;
-		
+
 		for (int i = 0; i < 1000; i++){
 			StartTime = System.nanoTime();
 			for (int j = 0; j < size; j++){
@@ -46,13 +48,13 @@ public class Part2 {
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+
 	private static double arrayListTime (int size) {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		long StartTime, endTime; 
+		long StartTime, endTime;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
-		
+
 		for (int i = 0; i < 1000; i++){
 			arrayList = new ArrayList<Integer>();
 			arrayList.ensureCapacity(size);
@@ -66,33 +68,46 @@ public class Part2 {
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+	///PART 2 B
+	 @SuppressWarnings("unchecked")
 	private static double arrayListTimeAndSum (int size) {
-		long StartTime, endTimeFill, endTimeAdd; 
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		long StartTime, endTimeFill, endTimeAdd;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
 		int sum = 0;
-		
+
 		for (int i = 0; i < 1000; i++){
+			arrayList = new ArrayList<Integer>();
+			arrayList.ensureCapacity(size);
 			StartTime = System.nanoTime();
+			Random rand = new Random();
 			for (int j = 0; j < size; j++){
-			//Array list
+			arrayList.add(ThreadLocalRandom.current().nextInt(0, 100));
 			}
 			endTimeFill = System.nanoTime();
-			//Array list and sum code
+			for (int k=0;k<size;k++ ) {
+			 Iterator<Integer> iterator = arrayList.iterator();
+
+			 while(iterator.hasNext()){
+				 int test = iterator.next().intValue();
+		  	 iterator.next();
+				 sum += test;
+			 }
+			}
 			endTimeAdd = System.nanoTime();
 			elapsedTimeSum += (endTimeAdd - StartTime)-(endTimeFill-StartTime);
 			denominator++;
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+
 	private static double vectorTime (int size) {
 		Vector<Integer> vector = new Vector<Integer>(size);
-		long StartTime, endTime; 
+		long StartTime, endTime;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
-		
+
 		for (int i = 0; i < 1000; i++){
 			vector = new Vector<Integer>();
 			StartTime = System.nanoTime();
@@ -105,27 +120,39 @@ public class Part2 {
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+ @SuppressWarnings("unchecked")
 	private static double vectorTimeAndSum (int size) {
-		long StartTime, endTimeFill, endTimeAdd; 
+		long StartTime, endTimeFill, endTimeAdd;
 		long elapsedTimeSum = 0;
 		int denominator = 0;
 		int sum = 0;
-		
+
 		for (int i = 0; i < 1000; i++){
 			StartTime = System.nanoTime();
+			Vector v;
+			v = new Vector(size);
 			for (int j = 0; j < size; j++){
-			//Vector code
+			Random rand = new Random();
+			int  n = rand.nextInt(50) + 1;
+			v.addElement(new Integer(n));
 			}
 			endTimeFill = System.nanoTime();
-			//Vector and sum code
+			for (int k=0;k<size;k++ ) {
+			 Iterator<Integer> iterator = v.iterator();
+
+			 while(iterator.hasNext()){
+				 int test = iterator.next().intValue();
+				 iterator.next();
+				 sum += test;
+			 }
+		 }
 			endTimeAdd = System.nanoTime();
 			elapsedTimeSum += (endTimeAdd - StartTime)-(endTimeFill-StartTime);
 			denominator++;
 		}
 		return elapsedTimeSum/denominator;
 	}
-	
+
 	public static void main(String[] args) {
 		int size = 10000;
 		System.out.println("Array Average Time: " + arrayTime(size) + " ns");
